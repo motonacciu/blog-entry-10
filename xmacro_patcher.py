9a3bc4e9fee4b01f9be4af95714cba6d09f425f7
Simone Pellegrini <simone.pellegrini@arm.com>
2014-03-30 17:08:13 +0100

    Auto-generation of parsing rules for bison specification file

--- xmacro_patcher.py
+++ xmacro_patcher.py
@@ -6,6 +6,7 @@ parser.add_argument('-t', dest='template', help='Template file')
 parser.add_argument('-o', dest='output', help='Output file')
 parser.add_argument('-c', dest='content', help='Generated content to replace')
 parser.add_argument('-r', dest='regex', help='Regular expression to match and replace')
+parser.add_argument('-j', dest='join', help="How to join content lines", default='\n')
 
 args = parser.parse_args()
 
@@ -18,13 +19,16 @@ else:
 save = []
 # clean up content 
 for line in content.split('\n'):
-    m = re.match("^#[^\n]*$", line, re.MULTILINE)
+    if len(line.strip()) == 0:
+        continue 
+
+    m = re.match("^#[^\n]*$", line.strip(), re.MULTILINE)
     if not m:
-        save.append(line)
+        save.append(line.strip())
 
 template = open(args.template).read()
 
-template = template.replace(args.regex, "\n".join(save))
+template = template.replace(args.regex, args.join.join(save))
 
 open(args.output, 'w').write(template)
 
